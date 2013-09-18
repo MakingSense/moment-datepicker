2329994102be812067160e399b6ccc64ea69d026
andresmoschini <andresmoschini@gmail.com>
2013-09-18 20:37:59 -0300

    Update d.ts references

--- Scripts/site.ts
+++ Scripts/site.ts
@@ -1,8 +1,8 @@
-/// <reference path="d.ts/underscore/underscore.d.ts" />
-/// <reference path="d.ts/moment/moment.d.ts" />
-/// <reference path="d.ts/knockout/knockout.d.ts" />
-/// <reference path="d.ts/jquery/jquery.d.ts" />
-/// <reference path="d.ts/bootstrap/bootstrap.d.ts" />
+/// <reference path="typings/underscore/underscore.d.ts" />
+/// <reference path="typings/moment/moment.d.ts" />
+/// <reference path="typings/knockout/knockout.d.ts" />
+/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/bootstrap/bootstrap.d.ts" />
 /// <reference path="../moment-datepicker/moment-datepicker.d.ts" />
 
 
